dc0a03fcbf39574c49e51ff164a170db6d1dc636
slipiduche <48814217+slipiduche@users.noreply.github.com>
2021-05-05 16:55:01 -0400

    home no data filterbuttons

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -1,7 +1,11 @@
+import 'dart:ui';
+
 import 'package:boat_monitor/Icons/icons.dart';
 import 'package:boat_monitor/bloc/authentication_bloc.dart';
+import 'package:boat_monitor/bloc/homeSearchBloc.dart';
 import 'package:boat_monitor/generated/l10n.dart';
 import 'package:boat_monitor/share_prefs/user_preferences.dart';
+import 'package:boat_monitor/styles/fontSizes.dart';
 import 'package:boat_monitor/styles/margins.dart';
 import 'package:boat_monitor/widgets/widgets.dart';
 import 'package:flutter/material.dart';
@@ -37,23 +41,91 @@ class _HomePageState extends State<HomePage> {
             SizedBox(
               height: 20.0,
             ),
-
             Expanded(
                 child: Column(
-              children: [_homeSearch(context)],
+              children: [
+                _homeSearch(context),
+                SizedBox(
+                  height: 10.0,
+                ),
+                StreamBuilder(
+                  stream: HomeSearchBloc().homeSearch,
+                  builder: (BuildContext context, AsyncSnapshot snapshot) {
+                    if (snapshot.hasData) {
+                      return Column(
+                        children: [
+                          _homeButtons(context),
+                          Container(child: Text(snapshot.data)),
+                        ],
+                      );
+                    } else {
+                      return Container(
+                        margin: EdgeInsets.symmetric(horizontal: marginExt1),
+                        child: Column(
+                          children: [
+                            _homeButtons(context),
+                            SizedBox(
+                              height: 20.0,
+                            ),
+                            Text(
+                              'No data',
+                              style:
+                                  TextStyle(color: blue1, fontSize: correoSize),
+                              textAlign: TextAlign.center,
+                            ),
+                            Divider(
+                              thickness: 1.0,
+                              color: gray1,
+                            )
+                          ],
+                        ),
+                      );
+                    }
+                  },
+                ),
+              ],
             )),
-            // StreamBuilder(
-            //   stream: PendingHomesBloc().pendingHomes,
-            //   builder: (BuildContext context, AsyncSnapshot snapshot) {
-            //     return Container();
-            //   },
-            // ),
           ],
         ),
       ),
       bottomNavigationBar: botomBar(1, context),
     ));
   }
+}
+
+Widget _homeButtons(BuildContext context) {
+  return Container(
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _filterButton(() {}, 'Salling'),
+        _filterButton(() {}, 'Arrived'),
+        _filterButton(() {}, 'unavailable'),
+        _filterButton(() {}, 'available')
+      ],
+    ),
+  );
+}
+
+Widget _filterButton(Function onTap, String text) {
+  return GestureDetector(
+    onTap: onTap,
+    child: Container(
+        height: 20.0,
+        width: 80.0,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              text,
+              style: TextStyle(color: gray1),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+        decoration: BoxDecoration(
+            color: gray, borderRadius: BorderRadius.circular(50.0))),
+  );
 }
 
 Widget _homeSearch(BuildContext context) {
@@ -75,7 +147,11 @@ Widget _homeSearch(BuildContext context) {
         Expanded(
             child: TextField(
           style: TextStyle(color: blue1),
-          decoration: InputDecoration(border: InputBorder.none,hintText: 'Type any boat name'),
+          decoration: InputDecoration(
+              border: InputBorder.none, hintText: 'Type any boat name'),
+          onChanged: (value) {
+            HomeSearchBloc().setHomeSearch = value;
+          },
         ))
       ],
     ),
